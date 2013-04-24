112efc2e69119fdbcbe0c986f650c04b00b63024
Ian Renton <ian@ianrenton.com>
2013-04-24 21:58:16 +0100

    Better error handling for the user and the admin.

--- canicallit.rb
+++ canicallit.rb
@@ -27,24 +27,28 @@ get '/' do
   if params.has_key?("search")
     term = params['search']
     matches = []
+    allServices = ['SourceForge', 'GitHub', 'Ruby Gems', 'PyPI', 'Maven', 'Debian', 'Fedora']
+    erroredServices = []
+    errors = []
 
     # Populate Matches array
-    findSourceForgeProjects(term, matches)
-    findGithubProjects(term, matches)
-    findRubyGems(term, matches)
-    findPyPIPackages(term, matches)
-    findMavenPackages(term, matches)
-    findDebianPackages(term, matches)
-    findFedoraPackages(term, matches)
+    findSourceForgeProjects(term, matches, erroredServices)
+    findGithubProjects(term, matches, erroredServices)
+    findRubyGems(term, matches, erroredServices)
+    findPyPIPackages(term, matches, erroredServices)
+    findMavenPackages(term, matches, erroredServices)
+    findDebianPackages(term, matches, erroredServices)
+    findFedoraPackages(term, matches, erroredServices)
 
-    # Mangle the Matches arrays as necessary
+    # Mangle the result arrays as necessary
     matches.uniq!
     matches.sort!{ |x,y| x[:name].downcase <=> y[:name].downcase }
     exactMatches = matches.select{ |match| (match[:exact] == true)}
     matches = matches - exactMatches
+    okServices = allServices - erroredServices
 
     # Generate results output using a template
-    generateResults(term, exactMatches, matches)
+    generateResults(term, exactMatches, matches, okServices, erroredServices)
   else
     # Generate main page output
     erb :index
@@ -58,10 +62,11 @@ not_found do
 end
 
 # Find Projects on SourceForge
-def findSourceForgeProjects(term, matches)
+def findSourceForgeProjects(term, matches, erroredServices)
   begin
     uri = URI.parse(SOURCEFORGE_API_PATH.sub('~', term))
     http = Net::HTTP.new(uri.host, uri.port)
+    http.read_timeout = 10
     request = Net::HTTP::Get.new(uri.request_uri)
     res = http.request(request)
     rhash = JSON.parse(res.body)
@@ -73,15 +78,19 @@ def findSourceForgeProjects(term, matches)
       maintainers = maintainers[0..-3]
       matches << {:name => rhash['Project']['name'], :by => maintainers, :url => rhash['Project']['summary-page'], :description => rhash['Project']['description'], :source => 'SourceForge', :exact => true}
     end
-  rescue
+  rescue Exception => e  
+    puts e.message  
+    puts e.backtrace.inspect 
+    erroredServices << 'SourceForge'
   end
 end
 
 # Find Projects on Github
-def findGithubProjects(term, matches)
+def findGithubProjects(term, matches, erroredServices)
   begin
     uri = URI.parse(GITHUB_API_PATH.sub('~', term))
     http = Net::HTTP.new(uri.host, 443)
+    http.read_timeout = 10
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(uri.request_uri)
@@ -94,15 +103,19 @@ def findGithubProjects(term, matches)
         break
       end
     end
-  rescue
+  rescue Exception => e  
+    puts e.message  
+    puts e.backtrace.inspect 
+    erroredServices << 'GitHub'
   end
 end
 
 # Find RubyGems
-def findRubyGems(term, matches)
+def findRubyGems(term, matches, erroredServices)
   begin
     uri = URI.parse(RUBYGEMS_API_PATH.sub('~', term))
     http = Net::HTTP.new(uri.host, 443)
+    http.read_timeout = 10
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(uri.request_uri)
@@ -111,44 +124,58 @@ def findRubyGems(term, matches)
     rhash.each do |rubygem|
       matches << {:name => rubygem['name'], :by => rubygem['authors'], :url => rubygem['project_uri'], :description => rubygem['info'], :source => 'Ruby Gems', :exact => (term.downcase == rubygem['name'].downcase)}
     end
-  rescue
+  rescue Exception => e  
+    puts e.message  
+    puts e.backtrace.inspect 
+    erroredServices << 'Ruby Gems'
   end
 end
 
 # Find PyPI Packages
-def findPyPIPackages(term, matches)
+def findPyPIPackages(term, matches, erroredServices)
   begin
     client = XMLRPC::Client.new2("#{PYPI_API_PATH}")
     client.http_header_extra = {"Content-Type" => "text/xml"}
+    client.timeout = 10
     result = client.call('search', {'name' => term})
     result.each do |package|
       matches << {:name => package['name'], :by => '', :url => "#{PYPI_API_PATH}/#{package['name']}", :description => package['summary'], :source => 'PyPI', :exact => (term.downcase == package['name'].downcase)}
     end
-  rescue
+  rescue Exception => e  
+    puts e.message  
+    puts e.backtrace.inspect 
+    erroredServices << 'PyPI'
   end
 end
 
 # Find Packages on Maven
-def findMavenPackages(term, matches)
+def findMavenPackages(term, matches, erroredServices)
   begin
     uri = URI.parse(MAVEN_API_PATH.sub('~', term))
     http = Net::HTTP.new(uri.host, uri.port)
+    http.read_timeout = 10
     request = Net::HTTP::Get.new(uri.request_uri)
     res = http.request(request)
     rhash = JSON.parse(res.body)
     rhash['response']['docs'].each do |package|
       matches << {:name => package['a'], :by => '', :url => "http://search.maven.org/#search%7Cga%7C1%7Ca%3A%22#{package['a']}%22", :description => '', :source => 'Maven', :exact => (term.downcase == package['a'].downcase)}
     end
-  rescue
+  rescue Exception => e  
+    puts e.message  
+    puts e.backtrace.inspect 
+    erroredServices << 'Maven'
   end
 end
 
 # Find Packages on Debian package database
-def findDebianPackages(term, matches)
+def findDebianPackages(term, matches, erroredServices)
   begin
     uri = URI.parse(DEBIAN_PACKAGES_PATH.sub('~', term))
-    source = Net::HTTP.get(uri)
-    doc = Nokogiri::HTML(source)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.read_timeout = 10
+    request = Net::HTTP::Get.new(uri.request_uri)
+    res = http.request(request)
+    doc = Nokogiri::HTML(res.body)
     # Search the results div for h3 blocks, these have the package names
     doc.xpath('//div[./@id="psearchres"]/h3').each do |h3|
       name = h3.content.sub(/^Package /, '')
@@ -158,30 +185,37 @@ def findDebianPackages(term, matches)
 
       matches << {:name => name, :url => url, :description => description, :source => 'Debian', :exact => (term.downcase == name.downcase)}
     end
-  rescue
+  rescue Exception => e  
+    puts e.message  
+    puts e.backtrace.inspect 
+    erroredServices << 'Debian'
   end
 end
 
 # Find Packages on Fedora PackageDB
-def findFedoraPackages(term, matches)
+def findFedoraPackages(term, matches, erroredServices)
   begin
     uri = URI.parse(FEDORA_PACKAGEDB_API_PATH.sub('~', term))
     http = Net::HTTP.new(uri.host, 443)
+    http.read_timeout = 10
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(uri.request_uri)
     res = http.request(request)
     rhash = JSON.parse(res.body)
-    if rhash['packageListings'].length > 0
+    if rhash.has_key?('packageListings') && rhash['packageListings'].length > 0
       package = rhash['packageListings'][0]
       matches << {:name => package['package']['name'], :by => package['owner'], :url => "https://admin.fedoraproject.org/pkgdb/acls/name/#{term}", :description => package['package']['summary'], :source => 'Fedora PackageDB', :exact => true}
     end
-  rescue
+  rescue Exception => e  
+    puts e.message  
+    puts e.backtrace.inspect 
+    erroredServices << 'Fedora'
   end
 end
 
 # Generate results output using a template
-def generateResults(term, exactMatches, matches)
+def generateResults(term, exactMatches, matches, okServices, erroredServices)
 
   if term == 'canicallit'
     heading = 'No.<br/><br/>That would just be rude.'
@@ -199,6 +233,6 @@ def generateResults(term, exactMatches, matches)
     end
   end
 
-  erb :results, :locals => {:term => term, :heading => heading, :summary1 => summary1, :summary2 => summary2, :summary3 => summary3, :exactMatches => exactMatches, :matches => matches}
+  erb :results, :locals => {:term => term, :heading => heading, :summary1 => summary1, :summary2 => summary2, :summary3 => summary3, :exactMatches => exactMatches, :matches => matches, :okServices => okServices, :erroredServices => erroredServices, :githubminwatchers => GITHUB_MIN_WATCHERS}
 
 end
